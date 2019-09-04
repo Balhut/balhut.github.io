83fab733446ba78abce1ae0132d78c6148d3ca91
Łukasz Żołądkiewicz <lukasz.zoladkiewicz@yologroup.pl>
2019-09-04 12:59:06 +0200

    test

---
+++ precache-manifest.ebd790c6fe0581907f468f5221d22ea9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3a67e35a7129219244a9da0dc970be7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e83e5ec4096923068446",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "fd4b360e773970337ae8",
+    "url": "/static/css/main.acf612fb.chunk.css"
+  },
+  {
+    "revision": "e83e5ec4096923068446",
+    "url": "/static/js/2.bdfee7d5.chunk.js"
+  },
+  {
+    "revision": "fd4b360e773970337ae8",
+    "url": "/static/js/main.c84dd323.chunk.js"
+  },
+  {
+    "revision": "636a12736d35f8ca98ed",
+    "url": "/static/js/runtime~main.7aca1543.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
